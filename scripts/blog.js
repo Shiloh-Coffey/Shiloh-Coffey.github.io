bc6096a371d84d386a37542d771601bbb5f9088c
Dillard "Shiloh" Coffey <90652599+Shiloh-Coffey@users.noreply.github.com>
2022-11-14 17:39:05 -0600

    Update blog.js

--- scripts/blog.js
+++ scripts/blog.js
@@ -1,5 +1,9 @@
 var blog_dict = [
   {
+    Title: "GraveyardCafe Site Published",
+    Paragraph: "I have finished and published this site! There are some minor things that I would like to tweak over time, like encoding the images on the website to base64. As it stands right now, I feel comfortable about putting this out and am proud of my work here.",
+  },
+  {
     Title: "GitHub JSON workaround",
     Paragraph: "Since GitHub allows limited use of pages when it comes to adding a form of CSV or JSON file I had to come up with some workaround for what you are currently looking at on screen. By creating a separate JavaScript file that holds a list, I can reference the variable and then load the posts from that. Then in another JavaScript file, I have the list loaded and appended to this HTML page. Ideally, this same function would be used for the 'Review' page that is also planned to be on this website, with minimal tweaks to the HTML code. Ideally some sort of score box that is positioned with a flex-box.",
   }
